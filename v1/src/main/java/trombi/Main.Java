8eac3bec2a2bdddb4c5d8f20859bab6bd6881581
Roland Koffi <koffiroland02@icloud.com>
2024-01-23 09:59:31 +0100

    Premier assemblage / ! \ Probleme librairie

---
+++ v1/src/main/java/trombi/Main.Java
@@ -0,0 +1,10 @@
+package camera;
+
+public class Main {
+    public static void main(String[] args) {
+        CameraManager cameraManager = new CameraManager();
+        CameraWindow cameraWindow = new CameraWindow(cameraManager);
+
+        
+    }
+}
