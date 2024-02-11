6178d41bf17d5be4a74ebd2d66d8647cd32b94bd
Pandadorable <geffroy250@gmail.com>
2024-02-11 15:43:41 +0100

    MariaDB in docker + Communication

---
+++ init_bdd.sh
@@ -0,0 +1,6 @@
+./bin/bash
+systemctl start docker.service
+sudo docker run --publish 3306:3306 --detach --name TrombiBDD --env MARIADB_ROOT_PASSWORD=trombipw  mariadb:latest
+sudo docker ps
+sudo docker exec -it TrombiBDD mariadb --user root -ptrombipw
+CREATE DATABASE DataBase;
