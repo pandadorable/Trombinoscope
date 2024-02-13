27a157c9f8466cae46a244ea2d18e8105a123586
Pandadorable <geffroy250@gmail.com>
2024-02-13 08:07:16 +0100

    Update init_bdd.sh

--- init_bdd.sh
+++ init_bdd.sh
@@ -3,4 +3,5 @@ systemctl start docker.service
 sudo docker run --publish 3306:3306 --detach --name TrombiBDD --env MARIADB_ROOT_PASSWORD=trombipw  mariadb:latest
 sudo docker ps
 sudo docker exec -it TrombiBDD mariadb --user root -ptrombipw
-CREATE DATABASE DataBase;
+CREATE DATABASE datatest;
+CREATE TABLE datatest.ELEVE ( nom TINYTEXT, prenom TINYTEXT);
