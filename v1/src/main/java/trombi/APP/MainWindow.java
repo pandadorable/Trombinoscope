4f6bb932d99d1aafb2d2f2c0cf0e697fe95bebe6
Roland Koffi <koffiroland02@icloud.com>
2024-02-15 14:55:21 +0100

    Ajout: MainWindow

---
+++ v1/src/main/java/trombi/APP/MainWindow.java
@@ -0,0 +1,34 @@
+package trombi.APP;
+
+import io.qt.widgets.QTabWidget;
+import io.qt.widgets.QWidget;
+import trombi.BDD.ImportXlsx;
+import trombi.CAMERA.CameraWindow;
+import trombi.PDF.GenPdf;
+
+public class MainWindow extends QWidget{
+
+    public MainWindow() {
+        
+       //Creer listTab
+       QTabWidget listTab = new QTabWidget(this);
+       listTab.resize(getMaximumSize());
+
+       //Creer tab camera
+       QWidget widCam = new QWidget();
+
+       //Ajouter la tab au Qtabwidg
+       listTab.addTab(widCam, "Caméra");
+
+       //Onlget Camera
+       CameraWindow cameraWindow = new CameraWindow(widCam);
+        
+        //Boutons pour le Xlsx et Pdf 
+        ImportXlsx btnXlsx = new ImportXlsx(listTab);
+        GenPdf btnPdf = new GenPdf(listTab, "v1/Trombi.pdf");
+
+        
+
+    }
+   
+}
