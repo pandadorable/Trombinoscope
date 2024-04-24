db070c6c7e415c6ca7c6a601964b512a7470adce
Pandadorable <geffroy250@gmail.com>
2024-04-25 00:40:56 +0200

    Update Main.java

--- v1/src/main/java/trombi/APP/Main.java
+++ v1/src/main/java/trombi/APP/Main.java
@@ -1,36 +1,24 @@
 package trombi.APP;
 
-import trombi.BDD.MariaDB;
-import trombi.PDF.pdf;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-
-import javax.imageio.ImageIO;
-
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.FileNotFoundException;
 
-import io.qt.widgets.*;
-
-public class Main 
+public class Main
     {
     public static void main(String[] args) throws FileNotFoundException, SQLException {
-        // Initialise l'application Qt
-        QApplication.initialize(args);
+        // // Initialise l'application Qt
+        // QApplication.initialize(args);
 
-        //Fenetre principale
-        MainWindow mainWindow = new MainWindow();
-        mainWindow.resize(800, 600);
-        mainWindow.show();
+        // //Fenetre principale
+        // MainWindow mainWindow = new MainWindow();
+        // mainWindow.resize(800, 600);
+        // mainWindow.show();
 
-        // Lance l'application
-        QApplication.exec();
-        QApplication.shutdown();
+
+        // // Lance l'application
+        // QApplication.exec();
+        // QApplication.shutdown();
+        MainWindow.main(args);
     }
 
 }
