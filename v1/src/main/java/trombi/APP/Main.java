a79ecaa3bbd08ba4e43bec2075f6725363d02af8
Pandadorable <geffroy250@gmail.com>
2024-05-26 15:42:12 +0200

    Update Main.java

--- v1/src/main/java/trombi/APP/Main.java
+++ v1/src/main/java/trombi/APP/Main.java
@@ -1,5 +1,6 @@
 package trombi.APP;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -10,9 +11,16 @@ public class Main
     {
     public static void main(String[] args) throws SQLException, IOException {
         MainWindow.main(args);
-        MariaDB.kill();
-        CameraWindow.kill();
+        MariaDB.kill(); // end connection with BDD
+        CameraWindow.kill(); // close all connection with webcams
+        cleanFiles("last_compressed_img.jpg"); //clean temporary file use for compression
         
         System.exit(0);
+    }
+
+    public static void cleanFiles(String file)
+    {
+        File myObj = new File(file);
+        myObj.delete();
     }
 }
